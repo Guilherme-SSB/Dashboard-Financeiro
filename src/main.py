51e2f7250b007f157426a4cf53d777c55c08dc3b
Guilherme <gui.samuel10@gmail.com>
2024-02-24 13:27:03 -0300

    add filtro de data

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-import dash
-from dash import dcc, html, Input, Output, State
-import pandas as pd
 import os
+from datetime import datetime
+
+import dash
 import mariadb
+import pandas as pd
+from dash import dcc, html, Input, Output
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -38,6 +40,14 @@ app.layout = html.Div(children=[
     ),
 
     html.H2(children='Comparação de Ações'),
+    dcc.DatePickerRange(
+        id='date-range',
+        min_date_allowed=min(df['DT_COTACAO']),
+        max_date_allowed=max(df['DT_COTACAO']),
+        initial_visible_month=max(df['DT_COTACAO']),
+        start_date=min(df['DT_COTACAO']),
+        end_date=max(df['DT_COTACAO'])
+    ),
     dcc.Dropdown(
         id='ticker-dropdown-2',
         options=[{'label': ticker, 'value': ticker} for ticker in df['TX_TICKER'].unique()],
@@ -95,34 +105,37 @@ def update_graph(selected_ticker):
     Output('comparison-graph', 'figure'),
     [Input('ticker-dropdown-2', 'value'),
      Input('ticker-dropdown-3', 'value'),
-     Input('time-interval', 'value')]
+     Input('time-interval', 'value'),
+     Input('date-range', 'start_date'),
+     Input('date-range', 'end_date')]
 )
-def update_comparison_graph(selected_ticker_1, selected_ticker_2, time_interval):
+def update_comparison_graph(selected_ticker_1, selected_ticker_2, time_interval, start_date, end_date):
     filtered_df_1 = df[df['TX_TICKER'] == selected_ticker_1]
     filtered_df_2 = df[df['TX_TICKER'] == selected_ticker_2]
 
-    # Agrupe os dados pelo intervalo de tempo escolhido
     filtered_df_1['DT_COTACAO'] = pd.to_datetime(filtered_df_1['DT_COTACAO'])
+    filtered_df_2['DT_COTACAO'] = pd.to_datetime(filtered_df_2['DT_COTACAO'])
+
+    # Filtrar o DataFrame por intervalo de data
+    filtered_df_1 = filtered_df_1[(filtered_df_1['DT_COTACAO'] >= start_date) & (filtered_df_1['DT_COTACAO'] <= end_date)]
+    filtered_df_2 = filtered_df_2[(filtered_df_2['DT_COTACAO'] >= start_date) & (filtered_df_2['DT_COTACAO'] <= end_date)]
+
+    # Agrupe os dados pelo intervalo de tempo escolhido
     filtered_df_1.set_index('DT_COTACAO', inplace=True)
     filtered_df_1 = filtered_df_1.resample(time_interval).ffill()
 
-    filtered_df_2['DT_COTACAO'] = pd.to_datetime(filtered_df_2['DT_COTACAO'])
     filtered_df_2.set_index('DT_COTACAO', inplace=True)
     filtered_df_2 = filtered_df_2.resample(time_interval).ffill()
 
     # Calcular os retornos das ações
-    returns_1 = filtered_df_1['NR_VL_COT_FECHAMENTO_AJUSTADO'].pct_change().dropna() * 100
-    returns_2 = filtered_df_2['NR_VL_COT_FECHAMENTO_AJUSTADO'].pct_change().dropna() * 100
-
-    # Ajustar os retornos para que ambas as séries comecem no mesmo ponto (0% de retorno no início)
-    returns_1_adjusted = returns_1 - returns_1.iloc[0]
-    returns_2_adjusted = returns_2 - returns_2.iloc[0]
+    returns_1 = filtered_df_1['NR_VL_COT_FECHAMENTO_AJUSTADO'].pct_change() * 100
+    returns_2 = filtered_df_2['NR_VL_COT_FECHAMENTO_AJUSTADO'].pct_change() * 100
 
     fig = {
         'data': [
-            {'x': returns_1.index, 'y': returns_1_adjusted, 'type': 'line',
+            {'x': returns_1.index, 'y': returns_1, 'type': 'line',
              'name': f'Retorno de {selected_ticker_1}'},
-            {'x': returns_2.index, 'y': returns_2_adjusted, 'type': 'line',
+            {'x': returns_2.index, 'y': returns_2, 'type': 'line',
              'name': f'Retorno de {selected_ticker_2}'},
         ],
         'layout': {
@@ -132,6 +145,7 @@ def update_comparison_graph(selected_ticker_1, selected_ticker_2, time_interval)
         }
     }
     return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
