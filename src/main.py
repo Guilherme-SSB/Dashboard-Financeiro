7be096ce67c3249e3c5ca06fc9258955fb3cc952
Guilherme <gui.samuel10@gmail.com>
2024-02-24 12:48:51 -0300

    dash inicial que cria grafico com preco de acao

---
+++ src/main.py
@@ -0,0 +1,62 @@
+import dash
+from dash import dcc, html, Input, Output
+import pandas as pd
+import os
+import mariadb
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Criando o aplicativo Dash
+app = dash.Dash(__name__)
+
+# Carregando dados de exemplo
+conn = mariadb.connect(user=os.getenv('MARIADB_USER'),
+                        password=os.getenv('MARIADB_PASSWORD'),
+                        host=os.getenv('MARIADB_HOST'),
+                        port=int(os.getenv('MARIADB_PORT')),
+                        database=os.getenv('MARIADB_DATABASE'))
+
+df = pd.read_sql('''
+SELECT A.ID_ATIVO, A.DT_COTACAO, A.NR_VL_COT_FECHAMENTO_AJUSTADO, B.TX_TICKER, B.ID_CATEGORIA FROM ATIVO.TCOTACAO_LISTADOS_B3 AS A
+JOIN ATIVO.TATIVOS_LISTADOS_B3 AS B ON A.ID_ATIVO = B.ID_ATIVO
+WHERE B.ID_CATEGORIA = 1 AND A.DT_COTACAO > '2023-01-01'
+''', conn)
+
+
+# Layout da aplicação
+app.layout = html.Div(children=[
+    html.H1(children='Gráfico de DataFrame'),
+    dcc.Dropdown(
+        id='ticker-dropdown',
+        options=[{'label': ticker, 'value': ticker} for ticker in df['TX_TICKER'].unique()],
+        value=df['TX_TICKER'].iloc[0],  # Valor padrão do dropdown
+        clearable=False,
+    ),
+    dcc.Graph(
+        id='example-graph',
+    )
+])
+
+# Callback para atualizar o gráfico com base na seleção do ticker
+@app.callback(
+    Output('example-graph', 'figure'),
+    [Input('ticker-dropdown', 'value')]
+)
+def update_graph(selected_ticker):
+    filtered_df = df[df['TX_TICKER'] == selected_ticker]
+    fig = {
+        'data': [
+            {'x': filtered_df['DT_COTACAO'], 'y': filtered_df['NR_VL_COT_FECHAMENTO_AJUSTADO'], 'type': 'line', 'name': 'Valor de Fechamento Ajustado'},
+        ],
+        'layout': {
+            'title': f'Gráfico para {selected_ticker}'
+        }
+    }
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
