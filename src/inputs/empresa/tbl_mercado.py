f70d8a5be09f15be140ff188dc8a1c95ed3137df
Guilherme <gui.samuel10@gmail.com>
2024-05-12 15:11:41 -0300

    cria tbl_mercado

--- src/inputs/empresa/tbl_mercado.py
+++ src/inputs/empresa/tbl_mercado.py
@@ -9,31 +9,23 @@ df = pd.read_csv('data/tbl_mercado.csv')
 df.reset_index(inplace=True)
 df.rename(columns={'index': 'ID'}, inplace=True)
 
-# adiciona a coluna DT_HR_ATUALIZACAO_UTC com a data e hora atual em UTC
-df['DT_HR_ATUALIZACAO_UTC'] = pd.to_datetime('now', utc=True)
+dict_cols = {j: i for i, j in enumerate(df.columns)}
 
-# TODO: se for pra usar iterrows, usar malandragem do tiktok
+# Recriar a tabela tbl_mercado
+sql_conn.execute_sql_command('DROP TABLE tbl_mercado')
+sql_conn.execute_sql_command('''
+CREATE TABLE tbl_mercado
+(
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    SIGLA_MERCADO VARCHAR(20) NOT NULL,
+    NM_MERCADO VARCHAR(100) NOT NULL,
+    DT_HR_ATUALIZACAO_UTC DATETIME NOT NULL
+);
+''')
 
-# copilot:
-
-# CREATE TRIGGER trg_insert_DT_HR_ATUALIZACAO_UTC
-# ON tbl_mercado
-# AFTER INSERT
-# AS
-# BEGIN
-#     UPDATE tbl_mercado
-#     SET DT_HR_ATUALIZACAO_UTC = GETUTCDATE()
-#     WHERE ID IN (SELECT ID FROM inserted)
-# END
-
-
-# muda o nome do index para ID
-
-print(df)
-
-# insere na seguinte tabela:
-# ID INT IDENTITY(1,1) PRIMARY KEY,
-#     SIGLA_MERCADO VARCHAR(20) NOT NULL,
-#     NM_MERCADO VARCHAR(100) NOT NULL,
-#     DT_HR_ATUALIZACAO_UTC DATETIME NOT NULL
-
+for row in df.values:
+    sigla = row[dict_cols['SIGLA_MERCADO']]
+    mercado = row[dict_cols['NM_MERCADO']]
+    insert_query = f"INSERT INTO tbl_mercado (SIGLA_MERCADO, NM_MERCADO, DT_HR_ATUALIZACAO_UTC) VALUES ('{sigla}', '{mercado}', GETUTCDATE())"
+    print('Inserindo mercado: ', sigla, '-', mercado)
+    sql_conn.execute_sql_command(insert_query)
