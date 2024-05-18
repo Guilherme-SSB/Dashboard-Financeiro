f0ab7f2b6f94b938a711b2bc2bf9426173b594cb
Guilherme <gui.samuel10@gmail.com>
2024-05-18 18:02:38 -0300

    refactor

--- src/main.py
+++ src/main.py
@@ -4,40 +4,11 @@ import dash_bootstrap_components as dbc
 from src.infra.sql_server import SQLServerConnection
 from src.models.db import DatabaseType
 
+# Instanciação das Conexões SQL
+SQL_CONN_EMPRESA = SQLServerConnection(database=DatabaseType.EMPRESA, windows_auth=True)
+SQL_CONN_ATIVO = SQLServerConnection(database=DatabaseType.ATIVO, windows_auth=True)
 
-def create_app():
-    # Inicializando o aplicativo Dash com tema escuro do Bootstrap
-    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-
-    @app.callback(
-        Output('table-cotacoes', 'columns'),
-        Output('table-cotacoes', 'data'),
-        Input('button-pesquisar', 'n_clicks'),
-        State('input-ativo', 'value')
-    )
-    def update_table(n_clicks, ativos):
-        if not ativos:
-            return [], []
-
-        # Transforme a lista ['PETR4', 'BPAC5'] em ('PETR4', 'BPAC5')
-        ativos_selected = tuple(ativos)
-
-        query = f"""SELECT B.TICKER, A.DT_COTACAO, A.VL_FECHAMENTO_AJUSTADO
-                    FROM tbl_cotacao_listados_b3 AS A
-                    JOIN tbl_ativos_listados_b3 AS B
-                    ON A.ID_ATIVO = B.ID
-                    WHERE B.TICKER IN {ativos_selected}
-                    """
-
-        df_cotacoes = SQL_CONN_ATIVO.select_data(query, return_as_dataframe=True)
-
-        # Retorna diretamente as colunas e os dados do DataTable
-        columns = [{'name': col, 'id': col} for col in df_cotacoes.columns]
-        data = df_cotacoes.to_dict('records')
-
-        return columns, data
-
-    return app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 
 def create_layout(app, df_ativos):
@@ -45,7 +16,7 @@ def create_layout(app, df_ativos):
     app.layout = html.Div([
         html.H1("Acompanhamento MF", className="header-title"),
 
-        # Formulário para pesquisar por uma tivo
+        # Formulário para pesquisar por um ativo
         html.Div([
             html.Label("Escolha o ativo"),
             dcc.Dropdown(
@@ -65,6 +36,51 @@ def create_layout(app, df_ativos):
     ])
 
 
+@app.callback(
+    Output('table-cotacoes', 'columns'),
+    Output('table-cotacoes', 'data'),
+    Input('button-pesquisar', 'n_clicks'),
+    State('input-ativo', 'value')
+)
+def update_table(n_clicks, ativos):
+    print(f"Ativos recebidos: {ativos}")  # Log de depuração para verificar os ativos recebidos
+
+    if not ativos:
+        return [], []
+
+    if len(ativos) == 1:
+        query = f"""SELECT B.TICKER, A.DT_COTACAO, A.VL_FECHAMENTO_AJUSTADO
+                    FROM tbl_cotacao_listados_b3 AS A
+                    JOIN tbl_ativos_listados_b3 AS B
+                    ON A.ID_ATIVO = B.ID
+                    WHERE B.TICKER = '{ativos[0]}'
+                    """
+
+    else:
+        # Transforme a lista ['PETR4', 'BPAC5'] em ('PETR4', 'BPAC5')
+        ativos_selected = tuple(ativos)
+
+        query = f"""SELECT B.TICKER, A.DT_COTACAO, A.VL_FECHAMENTO_AJUSTADO
+                    FROM tbl_cotacao_listados_b3 AS A
+                    JOIN tbl_ativos_listados_b3 AS B
+                    ON A.ID_ATIVO = B.ID
+                    WHERE B.TICKER IN {ativos_selected}
+                    """
+
+    print(f"Query executada: {query}")
+
+    df_cotacoes = SQL_CONN_ATIVO.select_data(query, return_as_dataframe=True)
+
+    # Verificar se a consulta SQL retornou resultados
+    print(f"Resultados da consulta: {df_cotacoes}")  # Log de depuração para verificar os resultados
+
+    # Retorna diretamente as colunas e os dados do DataTable
+    columns = [{'name': col, 'id': col} for col in df_cotacoes.columns]
+    data = df_cotacoes.to_dict('records')
+
+    return columns, data
+
+
 def get_data():
     # Recupera os ativos
     df_ativos_listados_b3 = SQL_CONN_ATIVO.select_data('SELECT * FROM tbl_ativos_listados_b3', return_as_dataframe=True)
@@ -72,10 +88,6 @@ def get_data():
 
 
 if __name__ == '__main__':
-    SQL_CONN_EMPRESA = SQLServerConnection(database=DatabaseType.EMPRESA, windows_auth=True)
-    SQL_CONN_ATIVO = SQLServerConnection(database=DatabaseType.ATIVO, windows_auth=True)
-
-    app = create_app()
     df_ativos = get_data()
     create_layout(app, df_ativos)
     app.run_server(debug=True)
