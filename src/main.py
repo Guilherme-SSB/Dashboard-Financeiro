360bdf286d8d6a1d55206cb421cea1a6246a8702
Guilherme <gui.samuel10@gmail.com>
2024-05-18 17:19:48 -0300

    refactor

--- src/main.py
+++ src/main.py
@@ -1,15 +1,46 @@
 import dash
+from dash import html, dcc
 import dash_bootstrap_components as dbc
-from dash import html
+from src.infra.sql_server import SQLServerConnection
+from src.models.db import DatabaseType
 
-# Inicializando o aplicativo Dash com tema escuro do Bootstrap
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-# Layout do aplicativo
-app.layout = html.Div([
-    html.H1("Acompanhamento Financeiro", className="header-title"),
-])
+def create_app():
+    # Inicializando o aplicativo Dash com tema escuro do Bootstrap
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+    return app
 
-# Rodando o aplicativo
+
+def create_layout(app, df_ativos):
+    # Layout do aplicativo
+    app.layout = html.Div([
+        html.H1("Acompanhamento MF", className="header-title"),
+
+        # Formulário para pesquisar por uma tivo
+        html.Div([
+            html.Label("Escolha o ativo"),
+            dcc.Dropdown(
+                id='ativo-dropdown',
+                options=[{'label': ativo, 'value': ativo} for ativo in df_ativos['TICKER'].unique()],
+                value=[],
+                multi=True,
+                className="dropdown"
+            ),
+        ], className="form-group"),
+    ])
+
+
+def get_data():
+    SQL_CONN_EMPRESA = SQLServerConnection(database=DatabaseType.EMPRESA, windows_auth=True)
+    SQL_CONN_ATIVO = SQLServerConnection(database=DatabaseType.ATIVO, windows_auth=True)
+
+    # Recupera os ativos
+    DF_ATIVOS_LISTADOS_B3 = SQL_CONN_ATIVO.select_data('SELECT * FROM tbl_ativos_listados_b3', return_as_dataframe=True)
+    return DF_ATIVOS_LISTADOS_B3
+
+
 if __name__ == '__main__':
+    app = create_app()
+    df_ativos = get_data()
+    create_layout(app, df_ativos)
     app.run_server(debug=True)
